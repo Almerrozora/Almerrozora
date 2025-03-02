399b0027edc88dc60a619cc74858df2ae43fb85c
Almerrozora <almerrozora@gmail.com>
2025-03-02 08:01:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+        function showLoveMessage() {
+            document.getElementById('message').innerText = "EVEN IF THE SKY FALLS I'D STILL BE STANDING BECAUSE THE ONLY THING THAT COULD TRULY BRING ME DOWN IS LOSING YOU.";
+        }
+    </script>
+</body>
+</html>
